754d0b331531038328f445643ba5d73f418cc654
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-21 15:07:41 +0300

    LINQ - Element Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Assignment.ListGenerator;
+﻿using System.Linq;
+using static Assignment.ListGenerator;
 
 namespace Assignment
 {
@@ -25,12 +26,44 @@ namespace Assignment
             #endregion
 
             #region 3. Return digits whose name is shorter than their value.
-            string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-
-            var Result = Arr.Where((str, index) => str.Length < index);
+            /// string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            /// 
+            /// var Result = Arr.Where((str, index) => str.Length < index);
             #endregion
 
-            Result.PrintAll();
+            //Result.PrintAll();
+            #endregion
+
+            #region LINQ - Element Operators
+
+            #region 1. Get first Product out of Stock 
+            /// var Result = ProductsList.First(p => p.UnitsInStock == 0);
+            /// 
+            /// Result = (from p in ProductsList
+            ///           where p.UnitsInStock == 0
+            ///           select p).First();
+            #endregion
+
+            #region 2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+            /// var Result = ProductsList.FirstOrDefault((p) => p.UnitPrice > 1000);
+            /// 
+            /// Result = (from p in ProductsList
+            ///           where p.UnitPrice > 1000
+            ///           select p).FirstOrDefault();
+            #endregion
+
+            #region 3. Retrieve the second number greater than 5 
+            /// int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 }; // 8
+            /// var Result = Arr
+            ///     .Where(num => num > 5)
+            ///     .ElementAt(1);
+            /// 
+            /// Result = (from num in Arr
+            ///          where num > 5
+            ///          select num).ElementAt(1);
+            #endregion
+
+            //Result.Print();
             #endregion
         }
     }
